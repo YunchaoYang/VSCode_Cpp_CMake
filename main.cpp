18992e08a0dc2ff56da280aa978b12aceb17bbf4
VoLinhTruc <linh.truc.yeu.doi@gmail.com>
2020-03-15 18:09:49 +0700

    commit from bico

--- main.cpp
+++ main.cpp
@@ -5,20 +5,6 @@ using namespace std;
 int main()
 {
     cout << 123;
-    cout << "\r\n";
-
-    int x;  
-
-    cin >> x;
-
-    cout << x;
-    cout << "add bla bla";
-    
-    ccou << "blo blo";
-
-    cout << "bleu bleu";
-
-    system("pause");
 
 
     return 0;
