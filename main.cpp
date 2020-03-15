481cbfe6afc1be59f313e9ceae3cae4854252846
VoLinhTruc <linh.truc.yeu.doi@gmail.com>
2020-03-15 13:09:19 +0700

    a little change

--- main.cpp
+++ main.cpp
@@ -5,16 +5,6 @@ using namespace std;
 int main()
 {
     cout << 123;
-    cout << 123;
-    cout << 123;
-
-    int x;
-
-    cin >> x;
-
-    cout << x;
-
-    system("pause");
 
     return 0;
 }
