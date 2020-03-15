1c0a313d2cfbe00ecda88c58a762b92177a0fea6
VoLinhTruc <linh.truc.yeu.doi@gmail.com>
2020-03-15 12:10:32 +0700

    Add all files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << 123;
+    cout << 123;
+    cout << 123;
+
+    int x;
+
+    cin >> x;
+
+    cout << x;
+
+    system("pause");
+
+    return 0;
+}
